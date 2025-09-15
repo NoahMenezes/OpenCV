3f14f8429fd54d6f000141dea852bcdb53623294
NoahMenezes <2006noahmenezes@gmail.com>
2025-09-15 19:57:29 +0530

    fixed the code

--- project1/security_cam.py
+++ project1/security_cam.py
@@ -3,16 +3,28 @@ import winsound
 
 # Open webcam
 webcam = cv2.VideoCapture(0)
-
-
+# Read the first frame
+_, im1 = webcam.read()
+im1=cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
 while True:
-    _, im1 = webcam.read()
     _, im2 = webcam.read()
-    diff = cv2.absdiff(im1, im2)
-    gray=cv2.cvtColor(diff, cv2.COLOR_BAYER_BG2GRAY)
-    _,thresh=cv2.threshold(gray,20,255, cv2.THRESH_BINARY)
+    im2_gray=cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+    
+    diff = cv2.absdiff(im1, im2_gray)
+    _,thresh=cv2.threshold(diff,20,255, cv2.THRESH_BINARY)
+    contours, _=cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for c in contours:
+        if cv2.contourArea(c)<5000:
+            continue
+        winsound.Beep(500, 100)
+        
+        
     cv2.imshow("Security camera", diff)
+    # Update the previous frame
+    im1 = im2_gray.copy()
     if cv2.waitKey(10)  ==27:
+        break
+    if cv2.getWindowProperty("Security camera", cv2.WND_PROP_VISIBLE) < 1:
         break
 
 # Cleanup
