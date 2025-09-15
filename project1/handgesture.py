2e4361f3fad6a99b8a8d95c933f870d3e9acfffe
NoahMenezes <2006noahmenezes@gmail.com>
2025-09-15 21:18:47 +0530

    completed with bugs

--- project1/handgesture.py
+++ project1/handgesture.py
@@ -8,7 +8,7 @@ drawing_option = mediapipe.solutions.drawing_utils
 screen_width, screen_height=pyautogui.size()
 # Start webcam
 camera = cv2.VideoCapture(0)
-
+x1=y1=x2=y2=0
 while True:
     _, image = camera.read()
     image_height, image_width, _=image.shape
@@ -29,16 +29,22 @@ while True:
             for id, lm in enumerate(one_hand_handmarks):
                 x=int(lm.x*image_width)
                 y=int(lm.y*image_height)
-                print(x,y)
                 if id==8:
                     mouse_x= (screen_width/image_width*x)
                     mouse_y=(screen_height/image_height*y)
                     cv2.circle(image,(x,y),10,(0,255,255))
                     pyautogui.moveTo(mouse_x, mouse_y)
+                    x1=x
+                    y1=y
+                    
                 if id==4:
+                    x2=x
+                    y2=y
                     cv2.circle(image, (x,y), 10, (0,255,255))
-                    
-                    
+        dist=y2-y1 
+        print(dist)   
+        if(dist<20):
+            pyautogui.click()
     # Show the webcam feed with drawings
     cv2.imshow("Hand movement video capture", image)
 
