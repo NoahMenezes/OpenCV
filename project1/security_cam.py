37011657ef99fec46635ca577dc0e81ed27f9d11
NoahMenezes <2006noahmenezes@gmail.com>
2025-09-15 19:41:09 +0530

    threshold binary added

--- project1/security_cam.py
+++ project1/security_cam.py
@@ -1,8 +1,20 @@
 import cv2
 import winsound
 
-webcam=cv2.VideoCapture(0)
+# Open webcam
+webcam = cv2.VideoCapture(0)
+
+
 while True:
-    _, im1=webcam.read(0)
-    _, im2=webcam.read(0)
-    
+    _, im1 = webcam.read()
+    _, im2 = webcam.read()
+    diff = cv2.absdiff(im1, im2)
+    gray=cv2.cvtColor(diff, cv2.COLOR_BAYER_BG2GRAY)
+    _,thresh=cv2.threshold(gray,20,255, cv2.THRESH_BINARY)
+    cv2.imshow("Security camera", diff)
+    if cv2.waitKey(10)  ==27:
+        break
+
+# Cleanup
+webcam.release()
+cv2.destroyAllWindows()
