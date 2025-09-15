ad96184f67db9bdd4acefea417460b4f25468ae4
NoahMenezes <2006noahmenezes@gmail.com>
2025-09-15 20:16:31 +0530

    x,y integer

--- project1/eye_controlled_mouse.py
+++ project1/eye_controlled_mouse.py
@@ -6,6 +6,7 @@ face_mesh_landmarks=mediapipe.solutions.face_mesh.FaceMesh(refine_landmarks=True
 cam=cv2.VideoCapture(0)
 while True:
     _,image=cam.read()
+    window_h, window_w, _=image.shape
     rgb_image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     processed_image=face_mesh_landmarks.process(rgb_image)
     all_face_landmark_points=processed_image.multi_face_landmarks
@@ -13,13 +14,17 @@ while True:
     if all_face_landmark_points:
         one_face_landmark_points=all_face_landmark_points[0].landmark
         for landmark_point in one_face_landmark_points:
-            print(landmark_point.x, landmark_point.y)
+            x=int(landmark_point.x*window_w)
+            y=int(landmark_point.y*window_h)
+            print(x,y)
+            
             
     cv2.imshow("Eye controlled mouse", image)
     key=cv2.waitKey(100)
     if key==27:
         break
-    
+    if cv2.getWindowProperty("Eye controlled mouse", cv2.WND_PROP_VISIBLE) < 1:
+        break
 cam.release()
 cv2.destroyAllWindows()
 
