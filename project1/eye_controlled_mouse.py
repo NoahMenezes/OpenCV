5f1eaf2add8cd1f01856cacbae9e97c6a3e4f0e1
NoahMenezes <2006noahmenezes@gmail.com>
2025-09-15 20:22:44 +0530

    eye fixed

--- project1/eye_controlled_mouse.py
+++ project1/eye_controlled_mouse.py
@@ -6,6 +6,7 @@ face_mesh_landmarks=mediapipe.solutions.face_mesh.FaceMesh(refine_landmarks=True
 cam=cv2.VideoCapture(0)
 while True:
     _,image=cam.read()
+    image=cv2.flip(image,1)
     window_h, window_w, _=image.shape
     rgb_image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     processed_image=face_mesh_landmarks.process(rgb_image)
@@ -13,12 +14,17 @@ while True:
     print(all_face_landmark_points)
     if all_face_landmark_points:
         one_face_landmark_points=all_face_landmark_points[0].landmark
-        for landmark_point in one_face_landmark_points:
+        for landmark_point in one_face_landmark_points[474:478]:
             x=int(landmark_point.x*window_w)
             y=int(landmark_point.y*window_h)
             print(x,y)
-            
-            
+            cv2.circle(image, (x,y), 3, (0,0,255))
+        left_eye=[one_face_landmark_points[145], one_face_landmark_points[159]]
+        for landmark_point in left_eye:
+            x=int(landmark_point.x*window_w)
+            y=int(landmark_point.y*window_h)
+            print(x,y)
+            cv2.circle(image, (x,y), 3, (0,0,255))
     cv2.imshow("Eye controlled mouse", image)
     key=cv2.waitKey(100)
     if key==27:
