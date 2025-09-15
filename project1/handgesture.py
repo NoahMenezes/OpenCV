d0059367af4304eb3bdd617374b3f9acb5ad18a9
NoahMenezes <2006noahmenezes@gmail.com>
2025-09-15 21:12:55 +0530

    handgesture for loop update and finger point update

--- project1/handgesture.py
+++ project1/handgesture.py
@@ -1,24 +1,52 @@
 import cv2
 import mediapipe
 import pyautogui
-capture_hands=mediapipe.solutions.hands.Hands()
-drawing_option=mediapipe.solutions.drawing_utils
-camera=cv2.VideoCapture(0)
+
+# Initialize Mediapipe Hands solution
+capture_hands = mediapipe.solutions.hands.Hands()
+drawing_option = mediapipe.solutions.drawing_utils
+screen_width, screen_height=pyautogui.size()
+# Start webcam
+camera = cv2.VideoCapture(0)
 
 while True:
-    _, image=camera.read()
-    image=cv2.flip(image,1)
-    rgb_image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    output_hands=capture_hands.process(rgb_image)
-    all_hands=output_hands.mult_hand_landmarks
+    _, image = camera.read()
+    image_height, image_width, _=image.shape
+    image = cv2.flip(image, 1)  # Mirror the image
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # Process image to detect hands
+    output_hands = capture_hands.process(rgb_image)
+
+    # Corrected attribute name here
+    all_hands = output_hands.multi_hand_landmarks
+
     if all_hands:
         for hand in all_hands:
+            # Draw hand landmarks on the image
             drawing_option.draw_landmarks(image, hand)
-            
+            one_hand_handmarks=hand.landmark
+            for id, lm in enumerate(one_hand_handmarks):
+                x=int(lm.x*image_width)
+                y=int(lm.y*image_height)
+                print(x,y)
+                if id==8:
+                    mouse_x= (screen_width/image_width*x)
+                    mouse_y=(screen_height/image_height*y)
+                    cv2.circle(image,(x,y),10,(0,255,255))
+                    pyautogui.moveTo(mouse_x, mouse_y)
+                if id==4:
+                    cv2.circle(image, (x,y), 10, (0,255,255))
+                    
+                    
+    # Show the webcam feed with drawings
     cv2.imshow("Hand movement video capture", image)
-    key=cv2.waitKey(100)
-    if key==27:
+
+    key = cv2.waitKey(100)
+    if key == 27:
         break
-    
+    if cv2.getWindowProperty("Hand movement video capture", cv2.WND_PROP_VISIBLE) < 1:
+        break
+# Cleanup
 camera.release()
 cv2.destroyAllWindows()
